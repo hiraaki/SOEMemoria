830235d7576437c9f6b75b5bd7f7f2133e13249d
Mauricio Ishida <mauricio_ishida@hotmail.com>
2018-08-06 09:36:50 -0300

    trace

--- src/Main.java
+++ src/Main.java
@@ -8,63 +8,11 @@ import java.util.Scanner;
 public class Main {
     public static Scanner scanner= new Scanner(System.in);
     public static void main(String[] args) throws ClassNotFoundException {
-        ArrayList<Trace> list = new ArrayList<>();
-//        File file = new File("src/file.txt");
+        long startTime = System.nanoTime();
+        Otimo otimo = new Otimo("src/bigone.trace",1024);
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime)/1000000;  //divide by 1000000 to get milliseconds.
+        System.out.println("Tempo de Load:"+duration);
 
-        File file = new File("src/bigone.trace");
-        BufferedReader reader = null;
-
-        try {
-            reader = new BufferedReader(new FileReader(file));
-            String text = null;
-
-            while ((text = reader.readLine()) != null) {
-                String array[]=text.split(" ");
-                list.add(new Trace(array[0],array[1]));
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (reader != null) {
-                    reader.close();
-                }
-            } catch (IOException e) {
-            }
-        }
-
-        int tamPagina = scanner.nextInt();
-        int nframe = scanner.nextInt();
-
-        int i = 0;
-        HashMap<String, ArrayList<Integer>> hashmap = new HashMap<>();
-
-        for (Trace trace : list) {
-            if(trace.getOperation()!= "W") {
-                String substring = new String(trace.getMemoryAccess()).substring(0,
-                        (int)(Math.log(tamPagina)/Math.log(2))
-                );
-//                System.out.println(trace.getMemoryAccess());
-//                System.out.println(substring);
-//                System.out.println((int)(Math.log(tamPagina)/Math.log(2)));
-                if (hashmap.containsKey(substring)) {
-                    hashmap.get(substring).add(i);
-                } else {
-                    ArrayList index = new ArrayList();
-                    index.add(i);
-                    hashmap.put(substring, index);
-                }
-            }
-            i++;
-        }
-//        int num = (Integer.parseInt("00a02b0", 16));
-//        System.out.println(Integer.toBinaryString(num));
-//        for (String key : hashmap.keySet()) {
-//            System.out.println(key+" "+Arrays.toString(hashmap.get(key).toArray()));
-//            //System.out.printf("%x -> %s\n", Integer.parseInt(key, 2), Arrays.toString(hashmap.get(key).toArray()));
-//        }
-        System.out.println("pronto");
     }
 }
---
+++ src/Otimo.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Otimo {
+
+    ArrayList<Trace> list;
+    HashMap<String, ArrayList<Integer>> hashmap;
+    int nFalhadePagina;
+    int tamPagina;
+
+    public Otimo (String filePath,int tamPagina){
+        this.list=new ArrayList<>();
+        this.hashmap= new HashMap<>();
+        int linha = 0;
+
+        File file = new File(filePath);
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String text = null;
+
+            while ((text = reader.readLine()) != null) {
+                String array[]=text.split(" ");
+                Trace trace =new Trace(array[0],array[1]);
+                this.list.add(trace);
+
+                if(trace.getOperation()!= "W") {
+                    String substring = new String(trace.getMemoryAccess()).substring(0,
+                            (int)(Math.log(tamPagina)/Math.log(2))
+                    );
+//                System.out.println(trace.getMemoryAccess());
+//                System.out.println(substring);
+//                System.out.println((int)(Math.log(tamPagina)/Math.log(2)));
+                    if (hashmap.containsKey(substring)) {
+                        hashmap.get(substring).add(linha);
+                    } else {
+                        ArrayList index = new ArrayList();
+                        index.add(linha);
+                        hashmap.put(substring, index);
+                    }
+                }
+                linha++;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+
+
+
+        for (Trace trace : list) {
+
+        }
+//        int num = (Integer.parseInt("00a02b0", 16));
+//        System.out.println(Integer.toBinaryString(num));
+//        for (String key : hashmap.keySet()) {
+//            System.out.println(key+" "+Arrays.toString(hashmap.get(key).toArray()));
+//            //System.out.printf("%x -> %s\n", Integer.parseInt(key, 2), Arrays.toString(hashmap.get(key).toArray()));
+//        }
+
+        System.out.println("Carregou");
+    }
+
+    public void executar(){
+        for(int i=0; i>this.list.size();i++){
+            
+        }
+    }
+
+
+
+
+}
